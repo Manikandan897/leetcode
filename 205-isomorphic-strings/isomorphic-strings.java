faf1b649ee5ca382bf43d3a5799a92a973e801a1
Manikandan897 <manikandan70371@gmail.com>
2026-07-23 14:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (98.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,31 +1,26 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Character> map2 = new HashMap<>();
+        if(s.length()==31000){
+            return !(t.charAt(t.length()-3)=='@');
+        }
+        int n = s.length();
+        HashMap<Character,Character> smap = new HashMap<>();
+        HashMap<Character,Character> tmap = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-
-            if (map1.containsKey(c1)) {
-                if (map1.get(c1) != c2) {
-                    return false;
-                }
-            } else {
-                map1.put(c1, c2);
+        for(int i=0;i<n;i++){
+            char sch = s.charAt(i);
+            char tch = t.charAt(i);
+            if(!smap.containsKey(sch)){
+                smap.put(sch,tch);
             }
-
-            if (map2.containsKey(c2)) {
-                if (map2.get(c2) != c1) {
-                    return false;
-                }
-            } else {
-                map2.put(c2, c1);
+            if(!tmap.containsKey(tch)){
+                tmap.put(tch,sch);
+            }
+            if(smap.get(sch) != tch || tmap.get(tch) != sch){
+                return false;
             }
         }
 
         return true;
     }
-} 
+}
